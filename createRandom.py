2ea1d24fafa93b2c2906304cb71c03bf71ac9044
suryachereddy <suryachereddy@gmail.com>
2022-11-12 14:38:05 -0700

    createRandom.py to create random layouts of custom size

---
+++ createRandom.py
@@ -0,0 +1,27 @@
+from random_maze.maze import Maze
+def main(w= 20, h= 20, n =1):
+    
+    for i in range(n):
+        a = Maze(w, h)
+        a.randomize()
+        mat = a._to_str_matrix()
+        res = ""
+        for row in mat:
+            res += "".join(row)
+            res += "\n"
+        # To Do: add agent start and end positions
+        # save res to lay file
+        testfile = open(f"layouts/random_{i}.lay","w")
+        testfile.write(res)
+
+if __name__ == "__main__":
+    # if arg multiple, create multiple mazes
+    # if arg -w, set width
+    # if arg -h, set height
+    import argparse
+    parser = argparse.ArgumentParser()
+    parser.add_argument("-width", "--width", type=int, default=20)
+    parser.add_argument("-height", "--height", type=int, default=20)
+    parser.add_argument("-n", "--number", type=int, default=1)
+    args = parser.parse_args()
+    main(args.width, args.height, args.number)
